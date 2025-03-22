4a16c065b807bba46d71d5b7e05f88abd243973f
minde8888 <58484005+minde8888@users.noreply.github.com>
2025-03-22 17:43:42 +0100

    stock linked list

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,15 +1,20 @@
-﻿#include <iostream>
-using namespace std;
+﻿using namespace std;
+#include <iostream>
 #include <string>
+#include <chrono>
 
 class Node {
 private:
-	int number;
+	string name;
+	int price;
+	int date;
 	Node* next;
 
 public:
-	Node(int value) {
-		this->number = value;
+	Node(string name, int price, int date) {
+		this->name = name;
+		this->price = price;
+		this->date = date;
 		this->next = nullptr;
 	}
 
@@ -24,52 +29,148 @@ public:
 		head = nullptr;
 	}
 
-	void addLast(int value) {
-		Node* newNode = new Node(value);
+	void addFirst() {
+		string name;
+		int price;
+		int date;
+		cout << "Enter product name: ";
+		cin >> name;
+		cout << "Enter product price: ";
+		cin >> price;
+		cout << "Enter product date (YYYYMMDD): ";
+		cin >> date;
+		Node* newNode = new Node(name, price, date);
+		newNode->next = head;
+		head = newNode;
+		cout << "Product added at the beginning." << endl;
+	}
+
+	void addLast() {
+		string name;
+		int price;
+		int date;
+		cout << "Enter product name: ";
+		cin >> name;
+		cout << "Enter product price: ";
+		cin >> price;
+		cout << "Enter product date (YYYYMMDD): ";
+		cin >> date;
+		Node* newNode = new Node(name, price, date);
 		if (head == nullptr) {
 			head = newNode;
+			cout << "Product added as the first element." << endl;
 			return;
 		}
-
 		Node* temp = head;
 		while (temp->next != nullptr) {
 			temp = temp->next;
 		}
 		temp->next = newNode;
+		cout << "Product added at the end." << endl;
 	}
 
 	void removeLast() {
+		if (head == nullptr) {
+			cout << "List is empty." << endl;
+			return;
+		}
+
 		Node* temp = head;
-		for (size_t i = 1; i < stoi(length())-1; i++) {
+		while (temp->next->next != nullptr) {
 			temp = temp->next;
 		}
 
 		temp->next = nullptr;
 	}
 
-	void addFirst(int value) {
-		Node* newNode = new Node(value);
-		newNode->next = head;
-		head = newNode;
-	}
-
 	void removeFirst() {
+		if (head == nullptr) {
+			cout << "List is empty." << endl;
+			return;
+		}
+
 		Node* temp = head;
-		temp->next = head->next->next;
-		head = temp;
+		head = head->next;
+		delete temp;
 	}
 
-	void addMiddle(int value) {
-		Node* newNode = new Node(value);
+	void search() {
+		if (head == nullptr) {
+			cout << "List is empty." << endl;
+			return;
+		}
 
-		int middle = stoi(length()) / 2;
+		string name;
+		cout << "Enter product name to search: ";
+		cin >> name;
 
 		Node* temp = head;
-		for (size_t i = 1; i < middle; i++) {
+		while (temp != nullptr)
+		{
+			if (name == temp->name) {
+				cout << "Product found: " << temp->name << " Price: " << temp->price
+					<< " Date: " << temp->date << endl;
+				head = temp;
+				temp->next = nullptr;
+				break;
+			}
 			temp = temp->next;
 		}
-		newNode->next = temp->next;
-		temp->next = newNode;
+	}
+
+	void removeAll() {
+		if (head == nullptr) {
+			cout << "List is already empty." << endl;
+			return;
+		}
+		cout << "Removing all products:" << endl;
+		print();
+
+		Node* temp = head;
+		head = nullptr;
+		cout << "All products have been removed." << endl;
+	}
+
+	void outOfDate() {
+		if (head == nullptr) {
+			cout << "List is empty." << endl;
+			return;
+		}
+
+		Node* temp = head;
+		while (temp != nullptr) {
+			if (soonOutOfDate(temp->date)) {
+				temp->name += " (OUT OF DATE)";
+			}
+			temp = temp->next;
+		}
+
+		cout << "Out of date products have been marked." << endl;
+	}
+
+	void orderByPrice() {
+		if (!head || !head->next) {
+			cout << "List is empty or has only one item." << endl;
+			return;
+		}
+
+		Node* first = head;
+		Node* last = nullptr;
+		while (first && first->next != last) {
+			Node* sorted = first->next;
+			for (Node* current = first; current->next != last; current = current->next) {
+				if (current->price > current->next->price) {
+					int temp = current->price;
+					current->price = current->next->price;
+					current->next->price = temp;
+					sorted = current->next;
+				}
+			}
+			last = sorted;
+		}
+		head = first;
+
+		cout << "Products sorted by price." << endl;
 	}
 
 	string length() {
@@ -83,39 +184,97 @@ public:
 	}
 
 	void print() {
+		if (head == nullptr) {
+			cout << "List is empty." << endl;
+			return;
+		}
 		Node* temp = head;
+		cout << "Products list:" << endl;
 		while (temp != nullptr) {
-			cout << temp->number << " ";
+			cout << "Name: " << temp->name
+				<< ", Price: " << temp->price
+				<< ", Date: " << temp->date << endl;
 			temp = temp->next;
 		}
-		cout << endl;
+	}
+
+	bool soonOutOfDate(int referenceDate) {
+
+		auto now = chrono::system_clock::now();
+
+		auto pastDay = now + chrono::hours(24);
+
+		time_t past_time_t = chrono::system_clock::to_time_t(pastDay);
+		tm localTime;
+		localtime_s(&localTime, &past_time_t);
+
+		int dateInt = (1900 + localTime.tm_year) * 10000 +
+			(localTime.tm_mon + 1) * 100 +
+			localTime.tm_mday;
+
+		if (referenceDate < dateInt) {
+			return true;
+		}
+
+		return false;
 	}
 };
 
 
+
 int main() {
 	LinkedList list;
+	int choice;
+	do {
+		cout << "\n*********************************" << endl;
+		cout << "[0] - Add First" << endl;
+		cout << "[1] - Add Last" << endl;
+		cout << "[2] - Search" << endl;
+		cout << "[3] - Remove First" << endl;
+		cout << "[4] - Remove Last" << endl;
+		cout << "[5] - Print List" << endl;
+		cout << "[6] - Order By Price" << endl;
+		cout << "[7] - Mark Out Of Date" << endl;
+		cout << "[8] - Remove All" << endl;
+		cout << "[9] - Exit" << endl;
+		cout << "Choose an option: ";
+		cin >> choice;
 
-	list.addFirst(8);
-	list.addFirst(18);
-
-	list.addLast(10);
-	list.addLast(20);
-	list.addLast(5);
-	list.addLast(30);
-
-	list.addMiddle(100);
-
-	list.removeFirst();
-	list.removeLast();
-
-	list.print();
-
-	cout << "Saraso ilgis: " << list.length() << endl;
+		switch (choice) {
+		case 0:
+			list.addFirst();
+			break;
+		case 1:
+			list.addLast();
+			break;
+		case 2:
+			list.search();
+			break;
+		case 3:
+			list.removeFirst();
+			break;
+		case 4:
+			list.removeLast();
+			break;
+		case 5:
+			list.print();
+			break;
+		case 6:
+			list.orderByPrice();
+			break;
+		case 7:
+			list.outOfDate();
+			break;
+		case 8:
+			list.removeAll();
+			break;
+		case 9:
+			cout << "Exiting program." << endl;
+			break;
+		default:
+			cout << "Invalid option. Please try again." << endl;
+			break;
+		}
+	} while (choice != 9);
 	return 0;
 }
-
-//1) Įterpimas į priekį
-//2) Ištrinimas iš galo
-//3) Ištrinimas iš priekio
-//4) Įterpimas į vidurį
